0310191dde6c1daac64f11480de17cb6c5d0a889
Paul Bagot <bagot221@campus.unicaen.fr>
2023-04-14 13:02:31 +0200

    canvas fini

--- gribouille/src/main/java/uit/gon/gribouille/App.java
+++ gribouille/src/main/java/uit/gon/gribouille/App.java
@@ -1,10 +1,14 @@
 package uit.gon.gribouille;
 
 import javafx.application.Application;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
@@ -14,14 +18,34 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private double prevX;
+    private double prevY;
 
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("CadreGribouille"), 640, 480);
+        Canvas dessin = (Canvas) scene.lookup("Canvas");
         stage.setScene(scene);
         stage.setMinHeight(scene.getHeight());
         stage.setMinWidth(scene.getWidth());
         stage.show();
+        stage.setOnCloseRequest( e -> {
+        	if(!Dialogues.confirmation()) {
+        		e.consume();
+        	}
+        });
+        
+        
+        dessin.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
+        	prevX = e.getX();
+        	prevY = e.getY();
+        });
+        
+        dessin.addEventHandler(MouseEvent.MOUSE_DRAGGED, e -> {
+        	dessin.getGraphicsContext2D().strokeLine(prevX, prevY, e.getX(), e.getY());
+        	prevX = e.getX();
+            prevY = e.getY();
+        });
     }
 
     static void setRoot(String fxml) throws IOException {
