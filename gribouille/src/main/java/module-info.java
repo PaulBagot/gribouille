810a28cf502ffc0d3417909cc65443f7ce4d4d13
Paul <paul.bagot@etu.unicaen.fr>
2023-06-08 00:12:39 +0200

    ajout export image + correction bug onKeyPressed du controleur

--- gribouille/src/main/java/module-info.java
+++ gribouille/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module uit.gon.gribouille {
 	requires java.desktop;
 	requires javafx.graphics;
 	requires javafx.base;
+	requires javafx.swing;
 
     opens uit.gon.gribouille.controleurs to javafx.fxml;
     exports uit.gon.gribouille;
